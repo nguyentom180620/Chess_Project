ae4d1b4245c7230885ed21ca7a4064458ebfa171
Tom <nguyentom@ufl.edu>
2024-06-24 14:40:02 -0400

    Added Green shape to demonstrate how position functions look; made window 1600x900

--- src/main.cpp
+++ src/main.cpp
@@ -2,8 +2,13 @@
 
 int main()
 {
-    auto window = sf::RenderWindow(sf::VideoMode(1920, 1080), "CMake SFML Project" );
+    auto window = sf::RenderWindow(sf::VideoMode(1600, 900), "CMake SFML Project");
+
     window.setFramerateLimit(144);
+
+    sf::CircleShape shape(50);
+    shape.setFillColor(sf::Color(100, 250, 50));
+    shape.setPosition(100, 100);
 
     while (window.isOpen())
     {
@@ -15,7 +20,8 @@ int main()
             }
         }
 
-        window.clear();
+        window.draw(shape);
         window.display();
+        window.clear();
     }
 }
